295ff9140352f0db6347818693e23e2873fe944c
matthias-debaets <matthias.debaets@student.ucll.be>
2023-07-23 19:26:59 +0200

    add xsplit pattern header file with 2 and 3D function declarations

---
+++ raytracer/raytracer/patterns/xsplit-pattern.h
@@ -0,0 +1,14 @@
+#pragma once
+
+# include "lambda-pattern.h"
+
+namespace raytracer
+{
+	namespace patterns
+	{
+		Pattern2D xsplit(double boundary);
+
+		Pattern3D xsplit(double boundary);
+	}
+}
+
