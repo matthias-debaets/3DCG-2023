f85ab92da51b78b708bc15eb2c46b66dc6e72ef1
matthias-debaets <matthias.debaets@student.ucll.be>
2023-07-24 12:54:48 +0200

    add 3d pattern ysplit just in case

--- raytracer/raytracer/patterns/ysplit-pattern.cpp
+++ raytracer/raytracer/patterns/ysplit-pattern.cpp
@@ -3,10 +3,22 @@
 using namespace raytracer;
 
 
-Pattern2D patterns::ysplit(double boundary)
+Pattern2D patterns::ysplit2d(double boundary)
 {
-	return make_pattern([boundary](const math::Point2D& point)
+	auto function = [boundary](const math::Point2D& point) -> bool
+	{
+		return point.y() < boundary;
+	};
+
+	return make_pattern(std::function<bool(const math::Point2D&)>(function));
+}
+
+Pattern3D patterns::ysplit3d(double boundary)
+{
+		auto function = [boundary](const math::Point3D& point) -> bool
 		{
-			return point.y() < boundary;
-		});
+					return point.y() < boundary;
+	};
+
+	return make_pattern(std::function<bool(const math::Point3D&)>(function));
 }
--- raytracer/raytracer/patterns/ysplit-pattern.h
+++ raytracer/raytracer/patterns/ysplit-pattern.h
@@ -6,8 +6,8 @@ namespace raytracer
 {
 	namespace patterns
 	{
-		Pattern2D ysplit(double boundary);
+		Pattern2D ysplit2d(double boundary);
 
-		//Pattern3D xsplit(double boundary);
+		Pattern3D ysplit3d(double boundary);
 	}
 }
