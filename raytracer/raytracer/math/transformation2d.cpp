ff2a7dfc9eb4ed7f5c03b63d2b85966d82137aca
matthias-debaets <matthias.debaets@student.ucll.be>
2023-08-17 00:14:54 +0200

    Add 2d transformations

--- raytracer/raytracer/math/transformation2d.cpp
+++ raytracer/raytracer/math/transformation2d.cpp
@@ -18,3 +18,19 @@ Transformation2D math::transformations::translation(const Vector2D& v)
 
     return Transformation2D(tm, itm);
 }
+
+Transformation2D math::transformations::rotate(Angle angle)
+{
+    Matrix3x3 tm = transformation_matrices::rotation(angle);
+    Matrix3x3 itm = transformation_matrices::rotation(-angle);
+
+    return Transformation2D(tm, itm);
+}
+
+Transformation2D math::transformations::scale(double sx, double sy)
+{
+	Matrix3x3 tm = transformation_matrices::scaling(sx, sy);
+	Matrix3x3 itm = transformation_matrices::scaling(1.0 / sx, 1.0 / sy);
+
+	return Transformation2D(tm, itm);
+}
